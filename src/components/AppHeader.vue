6666de8892e6eb28f67a4926fa790e80c8e0efc9
hamidreza.namaeimoghadam <hamidreza.namaeimoghadam@partdp.ir>
2025-08-02 14:20:59 +0330

    feat : add dark & light theme functions

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,13 +2,32 @@
       <header class="card">
     <h1>لیست وظایف </h1>
     
-    <button id="theme-switcher">
-      <img src="../../public/assets/images/icon-sun.svg" alt="Change color theme" />
+    <button id="theme-switcher" @click="changeTheme">
+      <img v-if="defaultTheme == 'dark'" :src= this.sunImage alt="Change color theme" />
+      <img v-else :src= this.moonImage alt="Change color theme" />
     </button>
-  </header>x``
+  </header>
 </template>
 <script>
 export default{
-    
+    data() {
+      return {
+        sunImage : "assets/images/icon-sun.svg",
+        moonImage : "assets/images/icon-moon.svg",
+        defaultTheme : 'dark',
+      }
+    },
+    methods:{
+      changeTheme(){
+        if (this.defaultTheme == "dark") {
+          this.defaultTheme = "light"
+          document.querySelector('body').classList.add('light')
+        }
+        else{
+          this.defaultTheme = "dark"
+          document.querySelector('body').classList.remove('light')
+        }
+      }
+    }
 }
 </script>
